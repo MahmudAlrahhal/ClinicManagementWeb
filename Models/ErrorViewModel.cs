abdb13159e5cf991891fd1b8e2dfdb947bc8c4e3
MahmudAlrahhal <Mahmud@DESKTOP-8S8D5IM>
2023-12-31 20:45:46 +0300

    N-Tier architecture was deployed in the solution.

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ClinicManagementWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
